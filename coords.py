a0a78f639e7ceb42473931e534fb4a86ebbaf197
Mia yun Ruse <mia@yunru.se>
2023-08-07 18:39:59 +0100

    Support coordinates
    needs documenting :)

--- coords.py
+++ coords.py
@@ -3,14 +3,80 @@ Parse coordinate arguments on the command-line.
 '''
 
 import argparse
+import re
+from math import ceil, floor, log, tan, pi
+from typing import Callable
+
+from helpers import exit
 
 import numpy as np
 
-def ints(string: str):
-    return map(int, string.split(','))
-
 OPTINT = dict(type=int, default=None)
-OPTINT2 = dict(type=ints, default=None)
+OPTINT2 = dict(default=None)
+
+COORD = r'''
+([-+]?)  # sign
+(?:
+    (\d+) \s* °  # degrees
+\s* (\d+) \s* [′']  # minutes
+\s* (?: (\d+ (?:\.\d+)?) \s* [″\"])?  # seconds
+|
+    (\d+ (?:\.\d+)? \s* °?)  # decimal
+)
+'''
+LETTER = r'\s*([NSEW])?'
+
+A = rf'^{COORD}{LETTER}[,\s]\s*{COORD}{LETTER}$'
+COORDINATES = re.compile(A, re.VERBOSE)
+
+def _process_coord(sign: str, degrees: str | None, minutes: str | None, seconds: str | None, decimal: str | None, letter: str):
+
+    if decimal is not None:
+        result = float(decimal)
+    else:
+        result = int(degrees)
+        result += int(minutes) / 60
+        result += int(seconds) / 3600
+
+    if sign == '-' or (letter or '') in 'SW':
+        result *= -1
+
+    return result, letter
+
+
+
+def parse_longlat(match: re.Match[str]):
+    """
+    Parse eg '47.61N 122.33W' into long,lat coords
+    Takes the results of COORDINATES.match
+    """
+    # TODO: parse e.g. 51° 30′ 26″
+    m: tuple[str | None] = match.groups()
+    a, a_letter = _process_coord(*m[:6])
+    b, b_letter = _process_coord(*m[6:])
+
+    if not a_letter and not b_letter:
+        # simple lat,long coords
+        return b, a
+    if not a_letter or not b_letter:
+        raise ValueError('Invalid compass directions')
+
+    if b_letter in 'NS':
+        if a_letter not in 'EW':
+            raise ValueError('Invalid compass directions')
+        return a, b
+    else:
+        if a_letter not in 'NS':
+            raise ValueError('Invalid compass directions')
+        return b, a
+
+
+def longlat_to_mercator(long: float, lat: float):
+    "Convert longitude and geodetic latitude (in degrees) to unscaled Web Mercator in [0, 1]"
+    return (
+        (long + 180) / 360,
+        (pi - log(tan(pi/4 + lat*pi/360))) / (pi * 2)
+    )
 
 def add_coordinate_options(parser: argparse.ArgumentParser):
     coords = parser.add_argument_group('coordinates', description='''\
@@ -42,7 +108,8 @@ def process_coordinate_niceties(args: argparse.Namespace):
     def exists(k):
         return get(k) is not None
 
-    def pair(xk, yk, yk_defaults_to_xk=False):
+    CoordFunc = Callable[[float | int], int] | None
+    def pair(xk: str, yk: str, yk_defaults_to_xk=False, coord_func: CoordFunc = None):
         "parse pairs of coordinate-like arguments"
         if not exists(xk):
             # clearly this mode isn't in use
@@ -51,7 +118,23 @@ def process_coordinate_niceties(args: argparse.Namespace):
             return
         
         XV = get(xk)
-        XV = list(XV)
+
+        match = COORDINATES.match(XV)
+        if coord_func is not None and match:
+            
+            # TODO: swap out coord_func for a nicer handler
+            # that works better with --radius?
+
+            # 38.904 N, 77.016 E
+            x, y = longlat_to_mercator(*parse_longlat(match))
+            set(xk, coord_func(x * 2**args.zoom))
+            set(yk, coord_func(y * 2**args.zoom))
+            return
+        
+        try:
+            XV = [int(x) for x in XV.split(',')]
+        except ValueError:
+            exit(2, f'Error in parsing: {XV!r} is not an integer')
 
         # cute parsing trick for eg `-x 7,4` `-x7 -y4`
         L = len(XV)
@@ -66,17 +149,14 @@ def process_coordinate_niceties(args: argparse.Namespace):
             # -W3 (square)
             set(yk, XV[0])
         else:
-            # -x "52.118N 2.325W"
-
             # -x7 -y4
             if not exists(yk):
                 raise TypeError(f'Cannot define -{xk} but not -{yk}')
-            # yk is already set
 
     # x,y parsing
-    args.x = args.x or [0]
+    args.x = args.x or 0
     args.y = args.y or 0
-    pair('x', 'y')
+    pair('x', 'y', coord_func=floor)
 
     # PIN      = any(es('x', 'y'))
     ABSOLUTE = exists('X') or exists('Y')
@@ -86,8 +166,8 @@ def process_coordinate_niceties(args: argparse.Namespace):
     N_MODES_USED = ABSOLUTE + RELATIVE + RADIUS
     if N_MODES_USED == 0:
         RELATIVE = True
-        args.W = [1]
-        args.H = 1
+        args.W = '1'
+        args.H = '1'
     elif N_MODES_USED > 1:
         exit(2, "Only one 'width mode' (X,Y / W,H / R) can be selected!")
 
@@ -99,7 +179,7 @@ def process_coordinate_niceties(args: argparse.Namespace):
         args.bound = np.array(bound, dtype=int).reshape((2, 2))
 
     if ABSOLUTE:
-        pair('X', 'Y')
+        pair('X', 'Y', coord_func=ceil)
         setbound('[x, y, X, Y]')
     elif RELATIVE:
         pair('W', 'H', yk_defaults_to_xk=True)
